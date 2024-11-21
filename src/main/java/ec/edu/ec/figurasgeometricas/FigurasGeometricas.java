4356ff08a3518bfc26e4592b7496fc3a77b23257
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-11-21 14:36:06 -0500

    Creacion del proyecto

---
+++ src/main/java/ec/edu/ec/figurasgeometricas/FigurasGeometricas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.figurasgeometricas;
+
+/**
+ *
+ * @author HP
+ */
+public class FigurasGeometricas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
